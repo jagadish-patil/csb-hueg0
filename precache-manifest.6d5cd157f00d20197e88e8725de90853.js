44304da72444cd463703fcd9d2171bc6ef11e9c6
Code Sandbox <hello@codesandbox.io>
2021-09-29 17:09:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6d5cd157f00d20197e88e8725de90853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf158b44508a42a4337738e9c41a5eb4",
+    "url": "https://jagadish-patil.github.io/csb-hueg0/index.html"
+  },
+  {
+    "revision": "888100ecab0d9b37ba64",
+    "url": "https://jagadish-patil.github.io/csb-hueg0/static/css/main.2942b4ab.chunk.css"
+  },
+  {
+    "revision": "b07f18d95fda6fe119a2",
+    "url": "https://jagadish-patil.github.io/csb-hueg0/static/js/2.601fda31.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://jagadish-patil.github.io/csb-hueg0/static/js/2.601fda31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "888100ecab0d9b37ba64",
+    "url": "https://jagadish-patil.github.io/csb-hueg0/static/js/main.95e5e8f8.chunk.js"
+  },
+  {
+    "revision": "14620928f5088d5bee13",
+    "url": "https://jagadish-patil.github.io/csb-hueg0/static/js/runtime-main.50f907ed.js"
+  }
+]);
